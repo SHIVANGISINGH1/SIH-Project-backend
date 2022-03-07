5b38855d4f49cd3853ef8c8d0adb9f5e82f2f07d
mighty_volcano <72347597+Harshkumar77@users.noreply.github.com>
2022-03-08 01:52:03 +0530

    maal masala commit

--- routes/routes-config.js
+++ routes/routes-config.js
@@ -1,7 +1,11 @@
 import contributeRoute from './contibute.js'
+import infoRoute from './info.js'
+import questionRoute from './question.js'
 
 function configureRoutes(app) {
   app.use(contributeRoute)
+  app.use(questionRoute)
+  app.use(infoRoute)
 }
 
 export default configureRoutes
