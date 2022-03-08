60c64198625540aa07593595776750907327a28f
mighty_volcano <72347597+Harshkumar77@users.noreply.github.com>
2022-03-09 00:09:53 +0530

    cors fix

--- index.js
+++ index.js
@@ -2,11 +2,21 @@ import express from 'express'
 import mongoose from 'mongoose'
 import configureRoutes from './routes/routes-config.js'
 import 'dotenv/config'
-
+import cors from 'cors'
 const app = express()
 
 mongoose.connect(process.env.MONGODB_URL, () => {
   console.log('Connected to mongoDB')
+})
+
+console.log(process.env.NODE_ENV)
+
+app.use((req, res, next) => {
+  if (process.env.NODE_ENV === 'development') {
+    res.header('Access-Control-Allow-Origin', '*')
+  }
+  res.header('Access-Control-Allow-Origin', process.env.CLIENT_URL)
+  next()
 })
 
 app.use(express.json())
