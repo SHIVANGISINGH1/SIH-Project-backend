27a811432060ad8febb4ec84dc97badf343f31fb
mighty_volcano <72347597+Harshkumar77@users.noreply.github.com>
2022-03-09 16:27:41 +0530

    somemore

--- routes/pdf-generate.js
+++ routes/pdf-generate.js
@@ -1,17 +1,23 @@
+import { renderFile } from 'ejs'
+import { render } from 'ejs'
 import { Router } from 'express'
 import { create } from 'html-pdf'
 import { createReadStream } from 'node:fs'
-import { dirname } from 'node:path'
 
 const router = Router()
 
-router.get('/api/generate/question-paper', (req, res) => {
-  res.header('Content-Type: application/pdf')
-  create('<h1>' + Date.now().toLocaleString() + '<h1>').toStream(
-    (err, stream) => {
-      stream.pipe(res)
-    }
+router.get('/api/generate/question-paper', async (req, res) => {
+  //   res.header('Content-Type: application/pdf')
+  const html = await renderFile(
+    '/home/volcano/P/sih-22/SIH-Project-backend/views/question-paper.ejs',
+    {
+      check: 'name',
+    },
+    { async: true }
   )
+  create(html).toStream((err, stream) => {
+    stream.pipe(res)
+  })
 })
 
 router.get('/api/generate/answer-key', (req, res) => {})
