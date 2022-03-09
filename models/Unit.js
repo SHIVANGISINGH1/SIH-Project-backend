8f5047fc6d89ae86615fe5659b9e5627b1104d79
mighty_volcano <72347597+Harshkumar77@users.noreply.github.com>
2022-03-09 23:48:50 +0530

    corrected unit model

--- models/Unit.js
+++ models/Unit.js
@@ -1,23 +1,24 @@
 import mongoose from 'mongoose'
-import Question from './question.js'
 
 const unitSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
   },
-
-  unitNumber: {
-    type: Number,
-    required: true,
-  },
-
   questions: [
     {
       type: mongoose.Types.ObjectId,
       ref: 'Question',
     },
-  ]
+  ],
+  Class: {
+    type: mongoose.Types.ObjectId,
+    ref: 'Class',
+  },
+  Subject: {
+    type: mongoose.Types.ObjectId,
+    ref: 'Subject',
+  },
 })
 
 const Unit = mongoose.model('Unit', unitSchema)
