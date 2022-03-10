2d1a7c6e74566c3beb1570148412ecbcafac797f
mighty_volcano <72347597+Harshkumar77@users.noreply.github.com>
2022-03-10 13:05:36 +0530

    browser giving bt with fetch :(

--- routes/Standard.js
+++ routes/Standard.js
@@ -3,7 +3,7 @@ import Class from '../models/class.js'
 
 const router = Router()
 
-router.get('/api/standard', async (req, res) => {
+router.post('/api/standard', async (req, res) => {
   // return the asked standard with populated subjects and unit
   try {
     const t = await Class.findById(req.body.standardId).populate({
